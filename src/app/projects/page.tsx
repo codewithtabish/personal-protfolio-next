17e1ff7aafa04b52a2bb7a3ab58db5703be0c492
codewithzain099 <zainiikhan099@gmail.com>
2024-10-03 09:07:20 -0700

    app basic commit of template

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,19 @@
+/** @format */
+
+import React from 'react';
+
+const ProjectHomePage = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque
+      nobis, quasi porro quidem laboriosam repellat rem fuga odio repudiandae,
+      veniam et magni eos iure, quia tempore repellendus? Provident,
+      voluptatibus porro? Lorem ipsum dolor sit, amet consectetur adipisicing
+      elit. Doloremque nobis, quasi porro quidem laboriosam repellat rem fuga
+      odio repudiandae, veniam et magni eos iure, quia tempore repellendus?
+      Provident, voluptatibus porro?
+    </div>
+  );
+};
+
+export default ProjectHomePage;
